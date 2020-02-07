06fd4071f9e4f2be785f63520bffe970f0d099b8
panmaciej <mwierzbicki666@gmail.com>
2020-02-07 01:46:56 +0100

    coinlist improvement

--- src/Components/CoinList/CoinList.js
+++ src/Components/CoinList/CoinList.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Coinlist.css";
 
-function CoinList() {
-  return <div className="m-coinlist-cointainer">CoinList</div>;
+class CoinList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?format=json&results=10")
+      .then(res => res.json())
+      .then(json => this.setState({ contacts: json.results }));
+  }
+  render() {
+    return <div className="m-coinlist-cointainer">CoinList</div>;
+  }
 }
 
 export default CoinList;
